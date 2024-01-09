80aca07f3f07d3838d5b0e3fba48037e95875ce2
motallebi.aliakbar <motallebi.aliakbar@gmail.com>
2024-01-09 09:35:54 -0800

    create home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        iranyekanbakh: ["iranyekanBakh"]
+      },
+      boxShadow: {
+        bxShadow: "0px 15px 30px 0px rgba(0, 0, 0, 0.05)",
+        shadowBtn: "0px 4px 25px 0px rgba(2, 132, 199, 0.3)",
+        numberShadow: "0px 4px 25px 0px rgba(2, 132, 199, 0.3)",
+        uploadBTN : ' rgba(255, 255, 255, 1)'
+      },
+
+      width: {
+        w168: "168px",
+        w264: "264px",
+        Inp4 : '290.5px'
+      },
+      height: {
+        h91: "91.96px",
+      },
+      
+      borderRadius: {
+        "ro2.5": "2.5rem",
+        "r14" : "14px"
+      },
+      backgroundImage: (theme) => ({
+
+      })
+    },
   },
   plugins: [],
 }
